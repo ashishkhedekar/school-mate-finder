b078ca5b3376a30647b0f480a5d096ca0ce25610
Ashish Khedekar <ashish.khedekar@wundermanthompson.com>
2021-11-14 23:05:37 +0000

    Filter based on the verified users

--- components/residentsDetails.js
+++ components/residentsDetails.js
@@ -1,15 +1,33 @@
-import styles from "../styles/Map.module.css";
+import styles from "../styles/Residents.module.css";
 import ResidentDetails from "./residentDetails";
+import {useState} from "react";
+
 
 const ResidentsDetails = ({residents}) => {
 
+  const [showOnlyVerifiedUser, setShowOnlyVerifiedUser] = useState(false);
+
+  const handleCheckboxChange = (e) => {
+    setShowOnlyVerifiedUser(e.target.checked)
+  }
+
   return <>
-    <div className={styles.school_list}>
-      {residents.map(resident => {
-        return <>
-          <ResidentDetails resident={resident}/>
-        </>
-      })}
+    <div className={styles.resident_containers}>
+      <div className="verifiedUserSelectorContainer">
+        <div className="verifiedUserSelector">
+          <input type="checkbox" id="userSelector" onChange={e => handleCheckboxChange(e)}/>
+          <div className="verifiedUserSelectorSlider"/>
+          <label className="verifiedUserSelector" htmlFor="userSelector"/>
+        </div>
+        <p>Show only verified </p>
+      </div>
+      <div className={styles.school_list}>
+        {residents
+          .filter(resident => !showOnlyVerifiedUser || (showOnlyVerifiedUser && resident.isVerified))
+          .map(resident => {
+          return (<ResidentDetails key={resident.id} resident={resident}/>)
+        })}
+      </div>
     </div>
   </>
 
